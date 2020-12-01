e11539ea2d8b42dd7c6a2c5803737147ad6db419
Abhiek187 <achaudhuri2011@yahoo.com>
2020-11-30 21:16:14 -0500

    Fill in missing features with 0 and return the true data

--- decision_trees.py
+++ decision_trees.py
@@ -7,7 +7,7 @@ def preprocess_data(data_frame):
     Convert the data set into a numerical data set with 2k features
 
     :param data_frame: the original data set as a pandas dataframe
-    :return: the preprocessed input data set as a numpy array
+    :return: the preprocessed input data set as a numpy array and the regular input as the output data set
     """
     # Prune any rows with missing data points (indicated by ?)
     filled_df = data_frame[~(data_frame == "?").any(axis=1)]
@@ -26,6 +26,7 @@ def preprocess_data(data_frame):
     # Convert the new dataframe into a numpy array
     data = filled_df.to_numpy()
     m = data.shape[0]
+    true_data = data
 
     # Augment the data set with flags indicating whether a feature is missing or not
     new_headers = list(filled_df)
@@ -43,19 +44,23 @@ def preprocess_data(data_frame):
             col = new_headers[c].split("_")[0]  # one-hot encodings start with header name_value
             missing_features[r][c] = features_dict[col]
 
+            if missing_features[r][c] == 0:
+                data[r][c] = 0  # replace missing features with 0
+
     # Interleave the data values with their missing flags (feature1, present?, feature2, present?, ...)
     x = np.empty((m, k2*2))
     # 0::2 = 0, 2, 4, ...; 1::2 = 1, 3, 5, ...
     x[:, 0::2] = data
     x[:, 1::2] = missing_features
-    return x
+    return x, true_data
 
 
 def main():
     # Read the CSV file and preprocess the data
     data_frame = pd.read_csv("adult.csv", encoding="utf-8", header=0)
-    x = preprocess_data(data_frame)
+    x, y = preprocess_data(data_frame)
     print(f"x[:10]: {x[:10]}")
+    print(f"y[:10]: {y[:10]}")
 
 
 if __name__ == "__main__":
